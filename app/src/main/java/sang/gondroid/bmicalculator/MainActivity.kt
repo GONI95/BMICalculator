3bb7c83e999b94d18c7bbe28706ef203d00bd5dd
GONI95 <sanggoni009@gmail.com>
2021-12-23 13:42:08 +0900

    Initial commit

---
+++ app/src/main/java/sang/gondroid/bmicalculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package sang.gondroid.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
