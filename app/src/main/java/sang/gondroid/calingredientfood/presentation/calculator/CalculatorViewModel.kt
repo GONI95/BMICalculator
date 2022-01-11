bcb5315bcf8325b2cca15ee29c092f057e0f039b
GONI95 <sanggoni009@gmail.com>
2022-01-12 03:31:47 +0900

    Feat : 검색 기능을 위한 EditText 기능 구현
    1. Soft Keyboard의 Done 버튼 클릭으로 EditText.setOnEditorActionListener로 인한 onEditorEnterAction() bindingAdapte 메서드가 CalculatorViewModel의 searchFunc() 고차함수를 매개변수로 받으며 호출됨
    2. Keyboard 숨김, 입력값이 빈 경우 처리 등의 처리 후 인자로 받은 searchFunc()을 호출
    3. CalculatorViewModel의 search() 메서드가 호출되고 내부에서 currentSearchMode() 고차함수를 호출하여 현재 Spinner에 선택된 Item과 대응되는 SearchMode를 받음
    
    Working: #5

--- app/src/main/java/sang/gondroid/calingredientfood/presentation/calculator/CalculatorViewModel.kt
+++ app/src/main/java/sang/gondroid/calingredientfood/presentation/calculator/CalculatorViewModel.kt
@@ -4,20 +4,37 @@ import android.view.View
 import sang.gondroid.calingredientfood.presentation.base.BaseViewModel
 import android.widget.AdapterView
 import sang.gondroid.calingredientfood.util.DebugLog
+import org.jetbrains.annotations.NotNull
+import sang.gondroid.calingredientfood.domain.util.SearchMode
 
 
 class CalculatorViewModel : BaseViewModel() {
     /**
-     * Gon [22.01.10] : 검색모드(SearchMode)를 담당하는 Spinner에서 Item 선택 시 호출
-     *                  parent.getCount() : Item 갯수
+     * Gon [22.01.11] : onEditorEnterAction() bindingAdapter 메서드의 매개변수로 전달되는
+     *                  반환값이 없는 하나의 인자를 갖는 고차함수
+     */
+    val searchFunc: (String) -> Unit = this::search
+
+    /**
+     * Gon [22.01.11] : 반환값을 가지는 인자를 갖지않는 고차함수
+     *                  반환값 : Spinner에 선택된 Item과 대응되는 SearchMode
+     */
+    private lateinit var selectSearchMode : () -> SearchMode
+
+    /**
+     * Gon [22.01.11] : 검색모드(SearchMode)를 담당하는 Spinner에서 Item 선택 시 호출
      *                  parent.getSelectedItem() : 선택한 Item
      */
     fun onSelectItem(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
-        DebugLog.v("$pos")
-        DebugLog.v("${parent.adapter.getItem(pos)}")
-        DebugLog.v("${parent.adapter.count}")
-        DebugLog.v("${parent.selectedItem}")
+        selectSearchMode = { parent.selectedItem as SearchMode }
     }
 
-
+    /**
+     * Gon [22.01.11] : onEditorEnterAction() bindingAdapter 메서드에서 호출되는 메서드
+     *                  매개변수 : EditText에 입력한 값
+     */
+    private fun search(@NotNull value: String) {
+        DebugLog.v(value)
+        DebugLog.v(selectSearchMode().name)
+    }
 }
--- app/src/main/java/sang/gondroid/calingredientfood/util/BindingAdapters.kt
+++ app/src/main/java/sang/gondroid/calingredientfood/util/BindingAdapters.kt
@@ -1,9 +1,17 @@
 package sang.gondroid.calingredientfood.util
 
+import android.content.Context
+import android.hardware.input.InputManager
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.databinding.BindingAdapter
 import androidx.viewpager2.widget.ViewPager2
 import me.relex.circleindicator.CircleIndicator3
 import android.widget.Spinner
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import sang.gondroid.calingredientfood.R
 import sang.gondroid.calingredientfood.domain.util.SearchMode
 import sang.gondroid.calingredientfood.presentation.widget.*
@@ -33,5 +41,36 @@ object BindingAdapters {
         val arrayAdapter = SpinnerAdapter(context, R.layout.item_search_mode, SearchMode.values())
         adapter = arrayAdapter
         setSelection(position)
+    }
+
+    /**
+     * Gon [22.01.11] : Soft Keyboard 완료 버튼 클릭 시 호출되는 메서드
+     *                  매개변수 : CalculatorViewModel의 고차함수 search
+     */
+    @JvmStatic
+    @BindingAdapter("onEditorEnterAction")
+    fun EditText.onEditorEnterAction(searchFunc: Function1<String, Unit>) {
+
+        setOnEditorActionListener { v, actionId, _ ->
+
+            // Gon [22.01.11] : InputMethodManager를 얻어와 키보드를 숨김
+            (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                .hideSoftInputFromWindow(v.windowToken, 0)
+
+            with(v.editableText) {
+
+                val imeAction = actionId == EditorInfo.IME_ACTION_SEARCH
+                val imeText = this.isNotBlank() or this.isNotEmpty()
+
+                /* Gon [22.01.11] : 입력값이 존재하고, 작업 식별자가 IME_ACTION_SEARCH를 만족하면, 입력값에서 공백을 제거하고
+                                    CalculatorViewModel search() 고차함수 호출
+                 */
+                if (imeText && imeAction) {
+                    true.also { searchFunc(this.toString().replace(" ","")) }
+                }
+                // Gon [22.01.11] : 입력값이 비어있는 경우 EditText에 Error message 표시
+                else false.also { error = resources.getString(R.string.please_enter_a_search_term) }
+            }
+        }
     }
 }
