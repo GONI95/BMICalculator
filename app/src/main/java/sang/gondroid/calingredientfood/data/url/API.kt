ae282c284a3842485f7017025b65d4249b053495
GONI95 <sanggoni009@gmail.com>
2022-01-11 14:06:16 +0900

    Feat : Http 통신 전 기본 설정
    OkHttp3, Retrofit2 라이브러리 종속성 추가
    INTERNET Permission 추가
    local.properties를활용으로 API Url, Key 숨기기
    
    Working: #5

---
+++ app/src/main/java/sang/gondroid/calingredientfood/data/url/API.kt
@@ -0,0 +1,9 @@
+package sang.gondroid.calingredientfood.data.url
+
+import sang.gondroid.calingredientfood.BuildConfig
+
+object API {
+    const val FNI_KEY = BuildConfig.FNI_API_KEY
+    const val FNI_URL = BuildConfig.FNI_API_URL
+    const val GET_FNI_LIST = "getFoodNtrItdntList1"
+}
