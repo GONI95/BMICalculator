cce87d7b6620c3a393d914d4f7b45c58cda537ad
GONI95 <sanggoni009@gmail.com>
2022-02-27 21:14:33 +0900

    Feat : NotificationSnackBar 주석 추가
    Resolve: #20

--- app/src/main/java/sang/gondroid/calingredientfood/presentation/widget/custom/NotificationSnackBar.kt
+++ app/src/main/java/sang/gondroid/calingredientfood/presentation/widget/custom/NotificationSnackBar.kt
@@ -8,14 +8,20 @@ import com.google.android.material.snackbar.Snackbar
 import sang.gondroid.calingredientfood.R
 import sang.gondroid.calingredientfood.databinding.SnackBarNotificationBinding
 
+/**
+ * Gon [22.02.26] : SnackBar는 final class 이므로 상속이 불가능, SnackBar의 getView()를 통해 View에 액세스해 커스텀하는 방법을 선택
+ */
 class NotificationSnackBar(view: View, private val message: String) {
 
+    // Gon [22.02.26] : 여러 instance로 메모리가 소비되지 않도록 SingleTon Pattern으로 구현
     companion object {
         fun make(view: View, message: String) = NotificationSnackBar(view, message)
     }
 
     private val context = view.context
+    // Gon [22.02.26] : LENGTH_INDEFINITE를 통해 다른 SnackBar가 표시되거나 dismiss하기 전까지 노출
     private val snackBar = Snackbar.make(view, "", Snackbar.LENGTH_INDEFINITE)
+    // Gon [22.02.26] : getView()를 통해 SnackBar의 View에 접근
     private val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
 
     private val inflater = LayoutInflater.from(context)
@@ -43,6 +49,7 @@ class NotificationSnackBar(view: View, private val message: String) {
     }
 
     fun show() {
+        // Gon [22.02.26] : SnackBar를 표시
         snackBar.show()
     }
 }
