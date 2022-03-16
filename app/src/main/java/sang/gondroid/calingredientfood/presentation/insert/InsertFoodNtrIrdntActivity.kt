8ba84e503895f0bb45db07b788e766ef9e7bb67f
GONI95 <sanggoni009@gmail.com>
2022-03-16 20:15:22 +0900

    Feat: create InsertFoodNtrIrdntActivity
    InsertFoodNtrIrdntActivity 생성
    
    Working: #30

---
+++ app/src/main/java/sang/gondroid/calingredientfood/presentation/insert/InsertFoodNtrIrdntActivity.kt
@@ -0,0 +1,12 @@
+package sang.gondroid.calingredientfood.presentation.insert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sang.gondroid.calingredientfood.R
+
+class InsertFoodNtrIrdntActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insert_food_ntr_irdnt)
+    }
+}
